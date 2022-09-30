e749f1fe0a8f311496d665c97ef81095c426a2d7
Samefral <galchun174@mail.ru>
2022-09-30 05:50:34 +0500

    getRandomInRange function added in main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// getRandomInRange источник: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_number_between_two_values
+
+const getRandomInRange = (min, max) => {
+  if (min <= 0 || max <= 0) {
+    return NaN;
+  }
+  return max < min ? getRandomInRange(max, min) : Math.random() * (max - min) + min;
+};
+
+getRandomInRange(13, 10);
