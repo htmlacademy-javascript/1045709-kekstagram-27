f91a15099e88526b54fd0ed18190df498317da7b
Samefral <galchun174@mail.ru>
2022-10-30 15:25:22 +0500

    double adding closePopupHandlers fixed

--- js/popup.js
+++ js/popup.js
@@ -5,41 +5,39 @@ const clearInputsInPopup = (popupContainer) => {
   allTextAreas.forEach((textArea) => (textArea.value = ''));
 };
 
-const addPopupHandlers = (popupHandlers) => {
-  popupHandlers.forEach((handlerObj) => handlerObj.target.addEventListener(handlerObj.type, handlerObj.func));
+const addPopupHandlers = (popupHandlersArr) => {
+  popupHandlersArr.forEach((handlerObj) => handlerObj.target.addEventListener(handlerObj.type, handlerObj.func));
 };
 
-const removePopupHandlers = (popupHandlers) => {
-  popupHandlers.forEach((handlerObj) => handlerObj.target.removeEventListener(handlerObj.type, handlerObj.func));
-};
-
-const closePopup = (popupContainer, popupHandlers, onClosePopupFunc) => {
-  popupContainer.classList.add('hidden');
-  document.body.classList.remove('modal-open');
-  removePopupHandlers(popupHandlers);
-  if (onClosePopupFunc) {
-    onClosePopupFunc();
-  }
+const removePopupHandlers = (popupHandlersArr) => {
+  popupHandlersArr.forEach((handlerObj) => handlerObj.target.removeEventListener(handlerObj.type, handlerObj.func));
 };
 
 const createPopupCloseHandlers = (popupContainer, closeBtn, popupHandlers, onClosePopupFunc) => {
 
-  const popupCloseClickHandler = () => {
-    closePopup(popupContainer, popupHandlers, onClosePopupFunc);
-  };
-
-  const popupCloseKeydownHandler = (evt) => {
-    if (evt.code === 'Escape' && document.activeElement.getAttribute('type') !== 'text' && document.activeElement.tagName !== 'TEXTAREA') {
-      closePopup(popupContainer, popupHandlers, onClosePopupFunc);
+  const closePopup = () => {
+    popupContainer.classList.add('hidden');
+    document.body.classList.remove('modal-open');
+    closeBtn.removeEventListener('click', popupCloseClickHandler);
+    document.removeEventListener('keydown', popupCloseKeydownHandler);
+    removePopupHandlers(popupHandlers);
+    if (onClosePopupFunc) {
+      onClosePopupFunc();
     }
   };
 
-  const popupCloseHandlers = [
-    {'target': closeBtn, 'type': 'click', 'func': popupCloseClickHandler},
-    {'target': document, 'type': 'keydown', 'func': popupCloseKeydownHandler}
-  ];
+  function popupCloseClickHandler() {
+    closePopup(popupContainer, popupHandlers, onClosePopupFunc);
+  }
 
-  popupHandlers.push(...popupCloseHandlers);
+  function popupCloseKeydownHandler(evt) {
+    if (evt.code === 'Escape' && document.activeElement.getAttribute('type') !== 'text' && document.activeElement.tagName !== 'TEXTAREA') {
+      closePopup(popupContainer, popupHandlers, onClosePopupFunc);
+    }
+  }
+
+  closeBtn.addEventListener('click', popupCloseClickHandler);
+  document.addEventListener('keydown', popupCloseKeydownHandler);
 };
 
 const showPopup = (popupContainer, closeBtn, popupHandlers = [], onClosePopupFunc = false) => {
