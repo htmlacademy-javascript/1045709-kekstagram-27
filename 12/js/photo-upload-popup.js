93045244ede21f32c566e5c6ee4314b1f300695d
Keks <keks@htmlacademy.ru>
2022-11-15 11:45:48 +0300

    :heavy_check_mark: Сборка #12

--- 12/js/photo-upload-popup.js
+++ 12/js/photo-upload-popup.js
@@ -1,24 +1,14 @@
 import { showModal, closeModal, addPopupCloseHandlers, removePopupCloseHandlers } from './popup.js';
-import { uploadForm, clearPristineErrors } from './photo-upload-validation.js';
-import { uploadImg, resetPhotoScale } from './photo-upload-scale.js';
-import { resetPhotoEffect, resetPhotoEffectSlider } from './photo-upload-effects.js';
+import { uploadReset } from './photo-upload-reset.js';
 
-const uploadFileInput = uploadForm.querySelector('#upload-file');
-const uploadPopup = uploadForm.querySelector('.img-upload__overlay');
-const uploadClosePopupBtn = uploadForm.querySelector('#upload-cancel');
+const uploadPopup = document.querySelector('.img-upload__overlay');
+const uploadClosePopupBtn = document.querySelector('#upload-cancel');
 
 
 const closeUploadPopup = () => {
   closeModal(uploadPopup);
   removePopupCloseHandlers(uploadClosePopupBtn, closePopupClickHandler, closePopupKeydownHandler);
-
-  uploadFileInput.value = '';
-  uploadForm.reset();
-
-  clearPristineErrors();
-  resetPhotoScale();
-  resetPhotoEffect();
-  resetPhotoEffectSlider();
+  uploadReset();
 };
 
 const showUploadPopup = () => {
@@ -36,13 +26,4 @@ function closePopupKeydownHandler(evt) {
   }
 }
 
-uploadImg.addEventListener('load', () => {
-  showUploadPopup();
-});
-
-uploadFileInput.addEventListener('change', () => {
-  const chosenFile = uploadFileInput.files[0];
-  uploadImg.src = URL.createObjectURL(chosenFile);
-});
-
-export { uploadPopup, closeUploadPopup, closePopupKeydownHandler };
+export { uploadPopup, showUploadPopup, closeUploadPopup, closePopupKeydownHandler };
